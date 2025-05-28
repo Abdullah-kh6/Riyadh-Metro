38cd43a7b19fcbe53519fa35b0fa20bd73d2099c
Abdullah <Abdullah.khawlani101@gmail.com>
2025-05-28 17:52:07 +0300

    Initial commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
 
+int add(int a, int b) => a + b;
+
 void main() {
-  test('sanity check', () {
-    expect(1 + 1, equals(2));
+  test('addition test', () {
+    expect(add(2, 3), 5);
   });
 }
