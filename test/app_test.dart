028833e4e8988038368b52a1d73037383ea6cec8
Abdullah-kh6 <abdullah.khawlani101@gmail.com>
2025-05-28 18:53:14 +0300

    Update app_test.dart

--- test/app_test.dart
+++ test/app_test.dart
@@ -1,14 +1,12 @@
-import 'package:riyadh_metro/payment/payment222/payment222_model.dart';
-import 'package:riyadh_metro/payment/p_a_y_m_e_n_t2_copy/p_a_y_m_e_n_t2_copy_model.dart';
-import 'package:riyadh_metro/payment/p_a_y_m_e_n_t2_copy2/p_a_y_m_e_n_t2_copy2_model.dart';
-import 'package:mime/mime.dart'; // For `mime()`
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:riyadh_metro/main.dart';
-import 'package:flutter/material.dart';
 
 void main() {
-  testWidgets('MyApp loads and shows a widget', (WidgetTester tester) async {
+  testWidgets('Riyadh Metro app launches correctly', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
+
+    // Confirm app launched by finding MaterialApp or home widget
     expect(find.byType(MyApp), findsOneWidget);
   });
 }
