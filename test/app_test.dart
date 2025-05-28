4bff54ca8613c103142a9f932293c8ec6ad46f88
Abdullah-kh6 <abdullah.khawlani101@gmail.com>
2025-05-28 18:38:24 +0300

    Update app_test.dart

--- test/app_test.dart
+++ test/app_test.dart
@@ -1,3 +1,7 @@
+import 'package:riyadh_metro/payment/payment222/payment222_model.dart';
+import 'package:riyadh_metro/payment/p_a_y_m_e_n_t2_copy/p_a_y_m_e_n_t2_copy_model.dart';
+import 'package:riyadh_metro/payment/p_a_y_m_e_n_t2_copy2/p_a_y_m_e_n_t2_copy2_model.dart';
+import 'package:mime/mime.dart'; // For `mime()`
 import 'package:flutter_test/flutter_test.dart';
 import 'package:riyadh_metro/main.dart';
 import 'package:flutter/material.dart';
